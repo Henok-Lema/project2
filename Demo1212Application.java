c6864bac1567b3ba780474e63538d00dd4fb2445
Henok-Lema <lemah8672@gmail.com>
2026-04-10 15:54:35 -0800

    new java

---
+++ Demo1212Application.java
@@ -0,0 +1,13 @@
+package com.example.demo1212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1212Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1212Application.class, args);
+	}
+
+}
